0cce7f0693396021c52d466a300213c08b48ec87
FlorentinoG9 <77408910+FlorentinoG9@users.noreply.github.com>
2024-12-30 22:52:25 +0100

    init linter antfu

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,9 @@
-import { z } from "zod";
+/* eslint-disable node/prefer-global/process */
+import { z } from 'zod'
 
 const env_schema = z.object({
-  NEXT_PUBLIC_SUPABASE_URL: z.string(),
+  NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
   NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
-});
+})
 
-export const env = env_schema.parse(process.env);
+export const env = env_schema.parse(process.env)
--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
-export const createSupabaseClient = () =>
-  createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  );
+import { env } from '@/env'
+import { createClient } from '@supabase/supabase-js'
+
+export function createSupabaseClient() {
+  return createClient(env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+}
