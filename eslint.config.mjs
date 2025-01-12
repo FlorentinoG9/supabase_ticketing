f9c1c03153fd0abcea8669dc89e0d449bbfc3a4f
FlorentinoG9 <77408910+FlorentinoG9@users.noreply.github.com>
2025-01-12 13:19:04 +0100

    update deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import antfu from '@antfu/eslint-config'
+import tailwind from 'eslint-plugin-tailwindcss'
 
 export default antfu(
   {
@@ -13,6 +14,28 @@ export default antfu(
       jsx: true,
       overrides: {
         'style/jsx-quotes': ['error', 'prefer-single'],
+      },
+    },
+  },
+  ...tailwind.configs['flat/recommended'],
+  {
+    settings: {
+      tailwindcss: {
+        callees: ['classnames', 'clsx', 'ctl', 'cva', 'cn', 'tw', 'twMerge'],
+        config: 'tailwind.config.ts',
+        cssFiles: [
+          '**/*.css',
+          '!**/node_modules',
+          '!**/.*',
+          '!**/dist',
+          '!**/build',
+        ],
+        cssFilesRefreshRate: 5_000,
+        removeDuplicates: true,
+        skipClassAttribute: false,
+        whitelist: [],
+        tags: [],
+        classRegex: '^class(Name)?$',
       },
     },
   },
